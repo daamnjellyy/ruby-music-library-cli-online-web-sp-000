7806d19c6bc692c6263f02239ae0258b5213a2bd
DANIELLE PARK <dpark1008@gmail.com>
2019-02-25 04:12:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,29 @@
 class Artist
   extend Concerns::Findable
-  attr_accessor :name :songs
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
+  attr_accessor :name, :songs
+
   @@all = []
 
-    def self.all
-      @@all
-    end
-
-    def initialize
-      save
-    end
-
-    def save
-      @@all << self
-    end
-
-    def self.reset_all
-      @@all.clear
-    end
-
-    def self.count
-      @@all.size
-    end
-
+  def initialize(name)
+    @name = name
+    @songs = []
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song unless songs.include?(song)
+    if song.artist == nil
+      song.artist = self
+    end
+  end
+
+  def genres
+    self.songs.collect{|song| song.genre}.uniq
+  end
+end
